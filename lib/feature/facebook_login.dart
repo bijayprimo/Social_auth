68c98d52fb58c37fce2a444a4548b04d2b0d0f52
test <Primo@123>
2023-05-03 17:39:08 +0530

    Changes

--- lib/feature/facebook_login.dart
+++ lib/feature/facebook_login.dart
@@ -8,7 +8,7 @@ class FacebookManager {
   Future<User?> signInWithFacebook() async {
     try {
       // Trigger the sign-in flow
-      final LoginResult loginResult = await FacebookAuth.instance.login();
+      final LoginResult loginResult = await FacebookAuth.instance.login(permissions: ['email']);
 
       print("login result ----- ${loginResult.message}");
 
