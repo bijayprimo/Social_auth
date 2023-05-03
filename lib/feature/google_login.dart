d59dac6338487fe61d23694f9ec712f5df47ceeb
test <Primo@123>
2023-05-03 16:17:29 +0530

    Changes

--- lib/feature/google_login.dart
+++ lib/feature/google_login.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -31,27 +32,7 @@ if(googleUser!= null){
         accessToken: googleAuth?.accessToken,
         idToken: googleAuth?.idToken,
       );
-      try {
-        final userCredent = await FirebaseAuth.instance.currentUser
-            ?.linkWithCredential(credential);
-      } on FirebaseAuthException catch (e) {
-        switch (e.code) {
-          case "provider-already-linked":
-            print("The provider has already been linked to the user.");
-            break;
-          case "invalid-credential":
-            print("The provider's credential is not valid.");
-            break;
-          case "credential-already-in-use":
-            print("The account corresponding to the credential already exists, "
-                "or is already linked to a Firebase User.");
-            break;
-        // See the API reference for the full list of error codes.
-          default:
-            print("Unknown error.");
-        }
-      }
-
+      FirebaseAuth.instance.currentUser!.linkWithCredential(credential);
 
       UserCredential userCredential =
       await FirebaseAuth.instance.signInWithCredential(credential);
